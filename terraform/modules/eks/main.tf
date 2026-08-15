7696a654cd71ebf9f80b7830e979c355626fd12c
oreoluwaonietan <oreoluwaonietan@gmail.com>
2026-08-15 14:03:05 +0000

    fix: pin admin access entry to root ARN instead of dynamic caller identity to prevent CI/local drift

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -32,17 +32,15 @@ module "eks" {
   }
 }
 
-data "aws_caller_identity" "current" {}
-
 resource "aws_eks_access_entry" "admin" {
   cluster_name  = module.eks.cluster_name
-  principal_arn = data.aws_caller_identity.current.arn
+  principal_arn = "arn:aws:iam::067514125712:root"
   type          = "STANDARD"
 }
 
 resource "aws_eks_access_policy_association" "admin" {
   cluster_name  = module.eks.cluster_name
-  principal_arn = data.aws_caller_identity.current.arn
+  principal_arn = "arn:aws:iam::067514125712:root"
   policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
 
   access_scope {
