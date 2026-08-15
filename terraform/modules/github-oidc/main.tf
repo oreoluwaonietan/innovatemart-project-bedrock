baeaae37bbe4ab948f14081a563c00049a3a39e6
oreoluwaonietan <oreoluwaonietan@gmail.com>
2026-08-15 13:34:12 +0000

    fix: use GitHub's known OIDC thumbprint instead of dynamically-fetched one

--- terraform/modules/github-oidc/main.tf
+++ terraform/modules/github-oidc/main.tf
@@ -1,11 +1,8 @@
-data "tls_certificate" "github" {
-  url = "https://token.actions.githubusercontent.com/.well-known/openid-configuration"
-}
-
 resource "aws_iam_openid_connect_provider" "github" {
   url             = "https://token.actions.githubusercontent.com"
   client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = [data.tls_certificate.github.certificates[0].sha1_fingerprint]
+  # GitHub's known OIDC thumbprint (AWS root CA validation makes this stable)
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
 
   tags = {
     Project = "tinyuka-2025-capstone"
