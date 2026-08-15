ab9c35fbf89c37d61d0fa79b2b15030a91bed10f
oreoluwaonietan <oreoluwaonietan@gmail.com>
2026-08-15 06:21:53 +0000

    feat: EKS cluster with managed node group and admin access entry

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,10 @@
 module "vpc" {
   source = "./modules/vpc"
 }
+
+module "eks" {
+  source = "./modules/eks"
+
+  vpc_id             = module.vpc.vpc_id
+  private_subnet_ids = module.vpc.private_subnets
+}
---
+++ terraform/modules/eks/main.tf
@@ -0,0 +1,51 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
+
+  cluster_name    = "project-bedrock-cluster"
+  cluster_version = "1.34"
+
+  cluster_endpoint_public_access = true
+
+  vpc_id     = var.vpc_id
+  subnet_ids = var.private_subnet_ids
+
+  cluster_enabled_log_types = [
+    "api", "audit", "authenticator", "controllerManager", "scheduler"
+  ]
+
+  authentication_mode = "API"
+
+  eks_managed_node_groups = {
+    bedrock_nodes = {
+      instance_types = ["t3.small"]
+      min_size       = 2
+      max_size       = 3
+      desired_size   = 2
+      capacity_type  = "ON_DEMAND"
+      subnet_ids     = var.private_subnet_ids
+    }
+  }
+
+  tags = {
+    Project = "tinyuka-2025-capstone"
+  }
+}
+
+data "aws_caller_identity" "current" {}
+
+resource "aws_eks_access_entry" "admin" {
+  cluster_name  = module.eks.cluster_name
+  principal_arn = data.aws_caller_identity.current.arn
+  type          = "STANDARD"
+}
+
+resource "aws_eks_access_policy_association" "admin" {
+  cluster_name  = module.eks.cluster_name
+  principal_arn = data.aws_caller_identity.current.arn
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+
+  access_scope {
+    type = "cluster"
+  }
+}
